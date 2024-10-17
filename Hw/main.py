77ab0610a4dd87e06b7931cd957b98908d709808
Arushay-Shahid <ummearushay567@gmail.com>
2024-10-17 17:35:24 +0500

    Add files via upload

---
+++ Hw/main.py
@@ -0,0 +1,10 @@
+marks=int(input("Enter marks of the student:"))
+if marks>=40:
+ print("You have passed the exam")
+ attendance=int(input("Enter attendance of the student:"))
+ if attendance>=70:
+  print("You are eligible for Board Examination.")  
+ else:
+      print("You are not ligible for Board Examination.")  
+else:
+   print("You will arrive in Resendup.")
